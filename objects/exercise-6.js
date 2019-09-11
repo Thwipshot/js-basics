04fe00bbd00dd7f03462d3ff2b32789b4b4330fb
Thwipshot <ethumpert@gmail.com>
2019-09-11 12:33:13 -0500

    created a simple price range object

---
+++ objects/exercise-6.js
@@ -0,0 +1,9 @@
+let priceRanges = [
+    { label: '$', tooltip: 'Inexpensive', minPerPerson: 0, maxPerPerson: 10},
+    { label: '$$', tooltip: 'Moderate', minPerPerson: 10, maxPerPerson: 25},
+    { label: '$$$', tooltip: 'expensive', minPerPerson: 25, maxPerPerson: 35}
+];
+
+let restaurants = [
+    { averagePerPerson: 5 }
+]
