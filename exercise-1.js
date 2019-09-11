fdbd1835ae1aec00bd46e2bb93794e0776faa426
Thwipshot <ethumpert@gmail.com>
2019-09-11 16:57:54 -0500

    created an array from range constructor

---
+++ exercise-1.js
@@ -0,0 +1,10 @@
+const numbers = arrayFromRange(-10, -4);
+
+function arrayFromRange(min, max) {
+    let output = [];
+    for( let i = min; i <= max; i++)
+        output.push(i);
+    return output; 
+}
+
+console.log(numbers);
